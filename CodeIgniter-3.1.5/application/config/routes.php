63fe8ef2d66968690aaa8fb7656676cd2bdb810e
Manoj <tvmmanu@gmail.com>
2017-07-13 18:11:27 +0530

    modified routes

--- CodeIgniter-3.1.5/application/config/routes.php
+++ CodeIgniter-3.1.5/application/config/routes.php
@@ -51,12 +51,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-/*$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;*/
+/*$route['default_controller'] = 'welcome';*/
+
 
 $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = TRUE;
